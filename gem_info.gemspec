820215d110065a6dd957e840d7966f57ee226bc6
George Ogata <george.ogata@gmail.com>
2011-12-28 11:28:34 -0500

    Use latest rspec.

--- gem_info.gemspec
+++ gem_info.gemspec
@@ -23,6 +23,6 @@ Gem::Specification.new do |s|
 
   s.specification_version = 3
   s.add_development_dependency 'ritual', '0.3.0'
-  s.add_development_dependency 'rspec'
+  s.add_development_dependency 'rspec', '~> 2.7.0'
   s.add_development_dependency 'mocha'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'gem_info'
-require 'spec'
-require 'spec/autorun'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.mock_with :mocha
 end
 
